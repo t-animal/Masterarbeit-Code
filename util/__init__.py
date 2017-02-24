d6f5d84529ebce418dfccad9098cef3aa8ceb977
Tilman Adler <tilman-git@t-animal.de>
2017-02-24 21:41:26 +0100

    (util) add utility class to check to which class a filename belongs

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,18 @@
 import numpy as np
+import os
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def isAroused(path):
+	""" Returns True if path points to a document which was written under aroused conditions
+	"""
+
+	folder, filename = os.path.split(os.path.abspath(path))
+
+	if folder.endswith("Veroff"):
+		return "C" in path
+
+	raise ValueError("Unknown path")
