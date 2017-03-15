7b692655eb0250f996a0edefda0245e4d3f6093f
Tilman Adler <tilman-git@t-animal.de>
2017-03-15 21:09:10 +0100

    (classifier) balance class weights

--- documentSum_SVM.py
+++ documentSum_SVM.py
@@ -49,7 +49,7 @@ class DocSumSVMClassifier(SVMClassifier):
 		yield fileSum
 
 	def train(self, trainFilenames):
-		super().train(trainFilenames, {"probability": self.min_probability > 0, "random_state": 42, "C": 2.5})
+		super().train(trainFilenames, {"probability": self.min_probability > 0, "random_state": 42, "C": 2.5, "class_weight": "balanced"})
 
 
 	def test(self, testFilenames):
