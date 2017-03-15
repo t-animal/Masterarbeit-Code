8ea2753efd10219ab19e7e3e1c6f045c28241886
Tilman Adler <tilman-git@t-animal.de>
2017-03-15 20:59:27 +0100

    (util) fix p-value calculation

--- util/containers.py
+++ util/containers.py
@@ -173,8 +173,7 @@ class TestresultContainer():
 	def oneline(self):
 		""" Returns a short oneliner describing the results"""
 		vals = self.getDict()
-		pValue = scipy.stats.binom.sf(vals["correct"], vals["tested"], 0.5) - \
-					 scipy.stats.binom.pmf(vals["correct"], vals["tested"], 0.5)/2
+		pValue = scipy.stats.binom.cdf(vals["incorrect"], vals["tested"], 0.5)
 		return "{} correct out of {} ({:6.2f}%, p={:6.3})".format(vals["correct"], vals["tested"],
 				                                                   vals["correct-percentage"], pValue)
 
