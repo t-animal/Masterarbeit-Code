bc4edc5c58ba2a507fbe0fea81463b4abdb39f26
Tilman Adler <tilman-git@t-animal.de>
2017-04-06 20:28:45 +0200

    (util) include p-value in output pdf

--- util/tablizeJSON.py
+++ util/tablizeJSON.py
@@ -1,6 +1,8 @@
 #!../venv/bin/python
 
 import json
+import numpy as np
+import scipy.stats
 import os
 import sys
 
@@ -10,6 +12,17 @@ from pylatex import Document, Section, Subsection, Tabular, MultiColumn,\
 from pylatex.basic import *
 from pylatex.utils import *
 from pylatex.package import *
+from pylatex.base_classes import *
+
+class Rotate(ContainerCommand):
+	def __init__(self, degrees, data=None, *args, **kwargs):
+		super().__init__(*args, data=data, **kwargs)
+
+		self.latex_name = "rotatebox"
+		self.arguments = [str(degrees)]
+		self.options = None
+		self.packages = [Package("graphicx")]
+
 
 def getAllResults(folder):
 	allResults = defaultdict(dict)
@@ -28,6 +41,21 @@ def getAllResults(folder):
 
 	return allResults
 
+def patchMissingPValue(allResults):
+	""" Fixes old json outputs, where nested CV p-values were not yet aggregated. """
+	if "p-value" in list(list(allResults.values())[0].values())[0]["nestedCVResult"]:
+		return allResults
+
+	for results in allResults.values(): #models
+		for result in results.values(): #datasets
+			pValues = [x["p-value"] for x in result["nestedCVResult"]["additional"]]
+			fisher = -2 * sum(np.log(pValues))
+			combinedPValue = scipy.stats.chi2.sf(fisher, 2 * len(result["nestedCVResult"]["additional"]))
+			result["nestedCVResult"]["p-value"] = combinedPValue
+
+	return allResults
+
+
 def getLatexDoc(results):
 	def groupSortingKey(key):
 		if "." in key:
@@ -42,9 +70,11 @@ def getLatexDoc(results):
 	bestLine = [0] * len(dataSets)
 	percentages = []
 	stddevs = []
+	pValues = []
 	for model, results in sorted(allResults.items(), key=lambda x: groupSortingKey(x[0])):
 		percentages.append([round(x[1]["nestedCVResult"]["correct-percentage-mean"], 2) for x in sorted(results.items())])
 		stddevs.append([round(x[1]["nestedCVResult"]["correct-percentage-stddev"], 2) for x in sorted(results.items())])
+		pValues.append([round(x[1]["nestedCVResult"]["p-value"], 4) for x in sorted(results.items())])
 
 	for lineno, line in enumerate(percentages):
 		for index, value in enumerate(line):
@@ -61,23 +91,27 @@ def getLatexDoc(results):
 	}
 	doc = Document("testoutput", geometry_options=geometry_options)
 	doc.packages.append(Package("FiraSans", options=["sfdefault"]))
+	# doc.packages.append(Package("graphicx"))
 	table = Tabular("l|ccccccc")
 
-	table.add_row([" "] + dataSets)
+	table.add_row([" "] + [Rotate(60, x) for x in dataSets])
 	table.add_hline()
 
-	for index, (percentages, stddevs) in enumerate(zip(percentages, stddevs)):
+	for index, (percentages, stddevs, pValues) in enumerate(zip(percentages, stddevs, pValues)):
 		if "." in models[index] and not models[index-1].endswith(models[index][-3:]):
 			table.add_empty_row()
 		table.add_row([MultiRow(2, data = models[index])] + [NoEscape(str(x) + "\%") for x in percentages])
-		table.add_row([""]+[NoEscape("$\pm$ " + str(x)) for x in stddevs])
+		# secondLine = map([SmallText() for x in percentages])
+
+		table.add_row([""]+[SmallText(NoEscape("$\pm$ {}, p: {}\%".format(s, round(p*100,2) if p < 0.1 else ">10"))) for s, p in zip(stddevs, pValues)])
 
 
 	doc.append(LargeText(table))
-	doc.generate_pdf(clean_tex=True)
+	doc.generate_pdf(clean_tex=False)
 
 
 if __name__ == "__main__":
-	allResults = getAllResults(sys.argv[1])
+	allResults = patchMissingPValue(getAllResults(sys.argv[1]))
+	# print(allResults)
 
 	getLatexDoc(allResults)
