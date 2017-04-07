4f3d4904141660d7d5f5805b1b79d1f1eb2d53b4
Tilman Adler <tilman-git@t-animal.de>
2017-04-07 16:55:32 +0200

    (classifier) add a simple bag of words classifier

---
+++ BagOfWordsClassifier.py
@@ -0,0 +1,66 @@
+#!./venv/bin/python
+
+import logging as log
+
+from util import isAroused
+from util.classifiers import Classifier, SVMClassifierMixin
+from util.containers import TestresultContainer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+class BagOfWordsClassifier(SVMClassifierMixin, Classifier):
+	"""A bag of words classifiers"""
+
+	def __init__(self, SVM_C = 2.5, gamma="auto", ngram_range=[1,1]):
+		self.SVM_C = SVM_C
+		self.gamma = gamma
+		self.ngram_range = [1,1]
+
+	def _generateDescribingVectors(self, filename):
+		a= self.vectorizer.transform([filename]).getrow(0).toarray()[0]
+		# import pdb
+		# pdb.set_trace()
+		yield a
+
+	def train(self, trainFilenames):
+		"""Trains the classifier with the given filenames
+
+		   :param trainFilenames: The filenames to train upon
+		   :type trainFilenames: iterable of strings
+		"""
+
+		self.vectorizer = TfidfVectorizer(input="filename", ngram_range=self.ngram_range)
+		self.vectorizer.fit(trainFilenames)
+
+		#this calls SVMClassifierMixin
+		super().train(trainFilenames, {"C": self.SVM_C,
+		                               "gamma": self.gamma,
+		                               "random_state": 42,
+		                               "class_weight": "balanced"})
+
+	def test(self, testFilenames):
+		"""Test the SVM using the files supplied in testFilenames.
+
+		   :param testFilenames: The filenames to test upon
+		   :type testFilenames: iterable of strings
+		   :returns: a TestresultContainer object
+		   :raises RuntimeError: if no svm was trained or loaded
+		"""
+		if self.svm is None:
+			raise RuntimeError("Call train or load before calling test!")
+
+		log.info("Beginning testing")
+		testResult = TestresultContainer(True, False, "aroused", "nonAroused")
+
+		for filename in testFilenames:
+			bow = self._getDescribingVectors(filename)[0]
+
+			result = self.svm.predict([bow])[0]
+			testResult.addResult(bool(result), isAroused(filename))
+
+			log.info("Checked file %s, result %s (%s)", filename, result,
+			         "CORRECT" if bool(result) == isAroused(filename) else "INCORRECT")
+
+		log.info("Finished testing: %s", testResult.oneline())
+
+		return testResult
