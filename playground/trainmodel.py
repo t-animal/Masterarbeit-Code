568e2e1e6ea42a1ced69812ba44f9fa40793c789
Tilman Adler <tilman-git@t-animal.de>
2017-05-08 23:22:24 +0200

    (playground) produce a working trainmodel script

--- playground/trainmodel.py
+++ playground/trainmodel.py
@@ -11,6 +11,7 @@ import os
 import pdb
 import re
 import logging as log
+import multiprocessing
 
 from pprint import pprint
 from gensim.models import Word2Vec
@@ -24,36 +25,31 @@ class WikiIterator():
 		self.texts = self.corpus.get_texts()
 		return self
 
-	def next(self):
-		return self.texts.next()
+	def __next__(self):
+		return list(map(lambda x: x.decode("utf-8"), next(self.texts)))
 
 
-def main(corpusPath, modelPath, overwrite=False, loadCorpus=False):
+def main(corpusPath, modelPath, dimensions, workers):
 	log.info("Started application")
 
 	if modelPath is None:
 		modelPath = datetime.now().strftime("data/model--%Y-%m-%d--%H:%M:%S")
 
 	log.info("Loading corpus")
-	if not loadCorpus:
-		corpus = g.corpora.wikicorpus.WikiCorpus(corpusPath)
-		corpus.save(corpusPath+"-precomputed")
-	else:
-		corpus = g.corpora.wikicorpus.WikiCorpus.load(corpusPath)
+	#provide an empty dictionary to suppress processing of the corpus to create it (we don't need it, only need the text). same for lemmatization
+	corpus = g.corpora.wikicorpus.WikiCorpus(corpusPath, lemmatize = False, dictionary = {})
 
-
-	newPath = ""
-	while os.path.exists(modelPath) and not overwrite and not newPath == "overwrite":
-		newPath = input("Path '{}' exists. Please enter a different path or 'overwrite': ".format(modelPath))
-		if not newPath == "overwrite":
-			modelPath = newPath
+	testIterator = iter(WikiIterator(corpus))
+	log.debug("Here are the first 100 words from the first two articles:")
+	log.debug(" ".join(list(next(testIterator))[:100]))
+	log.debug(" ".join(list(next(testIterator))[:100]))
 
 	log.info("Training model")
-	model = g.models.Word2Vec(WikiIterator(corpus), min_count=1)
+	model = g.models.Word2Vec(WikiIterator(corpus), size = dimensions, workers = workers)
 
 	log.info("Saving model")
 	model.save(modelPath)
-
+	model.wv.save_word2vec_format(modelPath + ".w2v")
 
 
 if __name__ == "__main__":
@@ -61,18 +57,23 @@ if __name__ == "__main__":
 	import argparse
 
 	parser = argparse.ArgumentParser(description='Train a word2vec model on a wikipedia corpus and save it to disc')
+	parser.add_argument("-v", help="Be more verbose (repeat v for more verbosity)", action="count", default=0)
+	parser.add_argument("--num",  "-n", help = "The maximum cpus to use (default: all). Leaves n cpus free if negative", type = int, default = multiprocessing.cpu_count())
 	parser.add_argument("--force", help="Force overwriting of existing model files", action="store_true")
-	parser.add_argument("-v", help="Be more verbose", action="store_true")
 	parser.add_argument("--modelPath", help="Where to save the word2vec model", default=None)
-	parser.add_argument("--loadCorpus", help="Load the corpus instead of creating it from scratch", action="store_true")
+	parser.add_argument("--dimensions", "-d", help="Dimensionality of the vectors to be learned", type=int, default=400)
 	parser.add_argument("corpusPath", help="Path to the wikipedia corpus to learn from.")
 
 	args = parser.parse_args()
 
+	if args.num <= 0:
+		args.num = multiprocessing.cpu_count() - max(1 - multiprocessing.cpu_count(), args.num)
+	args.num = min(args.num, multiprocessing.cpu_count())
+
 	log.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-					level=log.INFO if args.v else log.WARNING)
+				level=[log.WARN, log.INFO, log.DEBUG][min(args.v, 2)])
 
 	try:
-		main(args.corpusPath, args.modelPath, args.force, args.loadCorpus)
+		main(args.corpusPath, args.modelPath, args.dimensions, args.num)
 	except KeyboardInterrupt:
 		pass
