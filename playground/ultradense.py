c37d0603835709b7ee17f6d8ccd0d7183ee96bd7
Tilman Adler <tilman-git@t-animal.de>
2017-07-21 00:47:58 +0200

    add liwc training to ultradense

--- playground/ultradense.py
+++ playground/ultradense.py
@@ -18,7 +18,7 @@ from gensim.models import KeyedVectors
 """This is a playground file to test out rothe et al.'s paper "ultra dense word embeddings"
 """
 
-def batches(iterable, batchSize = 200):
+def batches(iterable, batchSize = 300):
 	batch = []
 	for item in iterable:
 		batch.append(item)
@@ -58,6 +58,9 @@ def seperateDifferentGroups(group1, group2, dimensions = 1, batchSize = 300, alp
 	assert(group1 and group2)
 	assert(group1[0].size == group2[0].size)
 
+
+	log.info("Beginning seperation of %d positive and %d negative words", len(group1), len(group2))
+
 	d = group1[0].size
 	dstar = dimensions
 	P = np.matrix(np.eye(dstar, d))
@@ -92,15 +95,19 @@ def seperateDifferentGroups(group1, group2, dimensions = 1, batchSize = 300, alp
 		assert((newQ * np.transpose(newQ) - np.identity(d) < 0.00001).all())
 		return newQ
 
+	log.info("Beginning optimization")
 
 	random.seed(42)
 	startTime = datetime.now()
 	startCost = sum(starmap(costFunction, itertools.product(group1, group2)))
+	batchCount = 0
 	for iteration in itertools.count(1):
 		LDiffGroup = list(itertools.product(group1, group2))
 		LSameGroup = list(itertools.combinations(group1, 2)) + list(itertools.combinations(group2, 2))
 		random.shuffle(LDiffGroup)
 		random.shuffle(LSameGroup)
+
+		log.info("Iteration no %d begun", iteration)
 
 		for ew, ev in LSameGroup:
 			assert(not all(ew == ev))
@@ -134,6 +141,7 @@ def seperateDifferentGroups(group1, group2, dimensions = 1, batchSize = 300, alp
 			#does the learn rate decrease per iteration or per batch? paper reads like per iteration, but that's weird
 			#judging from their code they draw exactly one random batch per iteration, i.e. it's per iteration
 			learnRate *= 0.99
+			batchCount += 1
 
 			if learnRate < abortRate:
 				break
@@ -143,10 +151,10 @@ def seperateDifferentGroups(group1, group2, dimensions = 1, batchSize = 300, alp
 
 	endTime = datetime.now()
 	totalCost = sum(starmap(costFunction, itertools.product(group1, group2)))
-	log.info("Terminating at after %d iterations over %d batches with termanation learnRate %f.", iteration, batchNo, learnRate)
+	log.info("Terminating at after %d iterations over %d batches with termanation learnRate %f.", iteration, batchCount, learnRate)
 	log.info("Training took %s", endTime - startTime)
-	log.info("Cost with random Q was %f (averaged over all pairs: %f)", startCost, startCost/len(group1)/len(group2))
-	log.info("Final total cost is %f (averaged over all pairs: %f)", totalCost, totalCost/len(group1)/len(group2))
+	log.info("Between-group cost with random Q was %f (higher is better) (averaged over all pairs: %f)", startCost, startCost/len(group1)/len(group2))
+	log.info("Final between-group cost is %f (higher is better) (averaged over all pairs: %f)", totalCost, totalCost/len(group1)/len(group2))
 
 	with open("final_Q-{}.pickle".format(datetime.now()), "wb") as file:
 		pickle.dump({
@@ -163,6 +171,89 @@ def seperateDifferentGroups(group1, group2, dimensions = 1, batchSize = 300, alp
 
 	return Q
 
+
+def trainWithNegativeSampling(positiveWordList, model, alpha=0.8):
+
+	frequentWords = model.index2word[:80000]
+	positiveVectors = [model[w] for w in positiveWordList if w in model and w in frequentWords]
+
+	random.seed(42)
+	random.shuffle(frequentWords)
+	negWordIterator = iter(frequentWords)
+
+	negativeVectors = []
+	while len(negativeVectors) < len(positiveVectors):
+		newWord = next(negWordIterator)
+		if newWord not in positiveWordList:
+			negativeVectors.append(model[newWord])
+
+	seperateDifferentGroups(positiveVectors, negativeVectors, alpha=alpha)
+
+def readLIWCList(fileName, categories, model = None):
+	""" Reads a LUIC list and extracts all words contained in the given categories.
+	    If model is given, words contained in the 800000 most frequent words in the model
+		are used to expand a prefix-word (ending with a *). E.g. "love*" will be expanded
+		to loveing, loves, lover if all these words are in the model
+
+		:param fileName: filename of the .dic file
+		:type fileName: string
+
+		:param categories: the categories of which to return the words
+		:type categories: list of int
+
+		:type model: if supplied, use the words in this model to expand an asterisk
+		:param model: gensim.models.KeyedVectors
+	"""
+
+	requestedCategories = set(categories)
+	words = []
+
+	with open(fileName) as file:
+		for line in file:
+			tokens = line[:-1].split("\t")
+			
+			if "0" <= tokens[0][0] <= "9" or tokens[0][0] == "%":
+				continue
+
+			try:
+				word, wordCategories = tokens[0], set([int(t) for t in tokens[1:]])
+			except ValueError:
+				#line contains weird stuff like <of> or ()
+				continue
+
+			if requestedCategories.intersection(wordCategories):
+				if not word[-1] == "*":
+					words.append(word)
+					continue
+					
+				if not model:
+					continue
+
+				for modelWord in model.index2word[:80000]:
+					if modelWord.startswith(word[:-1]) and not "_" in modelWord:
+						words.append(modelWord)
+
+	return words
+
+def getSentimentWords(positiveFilename, negativeFilename, model):
+	frequentWords = model.index2word[:80000]
+
+	posWords = [line[:-1] for line in open(positiveFilename)
+	                      if not line.startswith(";") and len(line) > 2 and line[:-1] in frequentWords]
+	negWords = [line[:-1] for line in open(negativeFilename)
+	                      if not line.startswith(";") and len(line) > 2 and line[:-1] in frequentWords]
+
+	random.seed(42)
+	random.shuffle(posWords)
+	random.shuffle(negWords)
+
+	posVec = [model[w] for w in posWords[:200]]
+	negVec = [model[w] for w in negWords[:200]]
+
+	posTestVec = [model[w] for w in posWords[201:401]]
+	negTestVec = [model[w] for w in negWords[201:401]]
+
+	return (posVec, negVec), (posTestVec, negTestVec)
 
 def checkResults(posVec, negVec, Q = None):
 	if Q is None:
@@ -203,53 +294,49 @@ def checkResults(posVec, negVec, Q = None):
 
 if __name__ == "__main__":
 
+	import argparse
 	import pickle
 	from gensim.models import KeyedVectors
 
+	parser = argparse.ArgumentParser(description='Use LIWC dictionary/sentiment lists to train ultradense subspaces')
+	parser.add_argument("--modelPath", "-m", help="Path to word2vec model", required=True)
+	parser.add_argument("--alpha", "-a", help="Weighting parameter", type=float, default=0.8)
+	parser.add_argument("--liwcDict", "-d", help="Path to liwc dictionary", default=None)
+	parser.add_argument("--liwcCat", "-c", help="List of liwc categories to use as positive samples", nargs="+", type=int, default=None)
+	parser.add_argument("--pos", "-p", help="Path to positive sentiment word list (if pos and neg are specified, train on sentiment)", default=None)
+	parser.add_argument("--neg", "-n", help="Path to negative sentiment word list (if pos and neg are specified, train on sentiment)", default=None)
+
+	args = parser.parse_args()
+	args.doLIWC = args.liwcDict and args.liwcCat
+	args.doSent = args.pos and args.neg
+
+	if args.doLIWC:
+		print("Performing optimization on LIWC dictionary categories")
+	if args.doSent:
+		print("Performing optimization on sentiment")
+	if not args.doLIWC and not args.doSent:
+		parser.error("Please specify either both -d and -c or both -p and -n or all four of them")
 
 	log.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
 	                level=log.DEBUG)
 
-	try:
-		with open("posVecs.pickle", "rb") as file:
-			posVec, posTestVec = pickle.load(file)
-		with open("negVecs.pickle", "rb") as file:
-			negVec, negTestVec = pickle.load(file)
-		log.info("Loaded vectors")
-	except Exception as e:
-		log.exception(e)
-		model = KeyedVectors.load_word2vec_format("/storage/MA/GoogleNews-vectors-negative300.bin", binary=True)
-		frequentWords = model.index2word[:80000]
+	model = KeyedVectors.load_word2vec_format(args.modelPath, binary=args.modelPath.endswith(".bin"))
 
-		posWords = [line[:-1] for line in open("/storage/MA/sentimentLexica/positive-words.txt")
-		                      if not line.startswith(";") and len(line) > 2 and line[:-1] in frequentWords]
-		negWords = [line[:-1] for line in open("/storage/MA/sentimentLexica/negative-words.txt")
-		                      if not line.startswith(";") and len(line) > 2 and line[:-1] in frequentWords]
+	if args.doLIWC:
+		log.info("Beginning training on liwc category")
+		words = readLIWCList(args.liwcDict, args.liwcCat)
 
-		random.seed(42)
-		random.shuffle(posWords)
-		random.shuffle(negWords)
+		log.info("Category words collected, begin training")
+		trainWithNegativeSampling(words, model, alpha=args.alpha)
 
-		posVec = [model[w] for w in posWords[:200]]
-		negVec = [model[w] for w in negWords[:200]]
+	if args.doSent:
+		(posVec, negVec), (posTestVec, negTestVec) = getSentimentWords(args.pos, args.neg, model)
 
-		posTestVec = [model[w] for w in posWords[201:401]]
-		negTestVec = [model[w] for w in negWords[201:401]]
+		Q = seperateDifferentGroups(posVec, negVec, alpha=args.alpha)
 
-		with open("posVecs.pickle", "wb") as file:
-			pickle.dump((posVec, posTestVec), file)
-		with open("negVecs.pickle", "wb") as file:
-			pickle.dump((negVec, negTestVec), file)
-
-	import sys
-	alpha = float(sys.argv[1])
-	print("Alpha set to " + str(alpha))
-	log.info("Beginning seperation of %d positive and %d negative words", len(posVec), len(negVec))
-	Q = seperateDifferentGroups(posVec, negVec, alpha=alpha)
-
-	print("Alpha set to " + str(alpha))
-	log.info("Checking results. This is the baseline: ")
-	checkResults(posTestVec, negTestVec)
-	log.info("Checking results. This is the result using the trained Q: ")
-	checkResults(posTestVec, negTestVec, Q)
+		log.info("Alpha set to %d", args.alpha)
+		log.info("Checking results. This is the baseline: ")
+		checkResults(posTestVec, negTestVec)
+		log.info("Checking results. This is the result using the trained Q: ")
+		checkResults(posTestVec, negTestVec, Q)
 
